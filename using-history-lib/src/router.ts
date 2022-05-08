53c27f0f473478e3507dbfb647fd876069ec622c
leejss <goldemshine@gmail.com>
2022-05-08 15:06:17 +0900

    🔥

---
+++ using-history-lib/src/router.ts
@@ -0,0 +1,35 @@
+import history from "./history";
+
+type Component = () => void;
+
+const createRouter = () => {
+  const routeMap = new Map();
+  let prevPathname: string | null = null;
+  let notFound = () => null;
+
+  const router = {
+    addRoutes(url: string, comp: Component) {
+      routeMap.set(url, comp);
+      return this;
+    },
+    setNotFound(c: any) {
+      notFound = c;
+      return this;
+    },
+    start() {
+      history.listen(({ action, location }) => {
+        if (location.pathname !== prevPathname) {
+          const currentRoute = routeMap.get(location.pathname);
+          if (currentRoute) {
+            currentRoute();
+          } else {
+            notFound();
+          }
+        }
+      });
+    },
+  };
+  return router;
+};
+
+export default createRouter;
